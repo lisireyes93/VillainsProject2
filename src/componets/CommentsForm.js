ae6fa37f55cc5421d97838aa8a9d61c3c2052efe
Bryan Gutierrez <gutierrezbryan32@yahoo.com>
2021-08-25 19:13:36 -0400

    finished post

--- src/componets/CommentsForm.js
+++ src/componets/CommentsForm.js
@@ -1,30 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
 
 function CommentsForm({ villainsToComments }) {
     console.log(villainsToComments)
+
+    const [comments, setComments] = useState([]);
+    const [villainSelected, setVillainSelected] = useState(`Abomination`);
+    const [commentContent, setcommentContent] = useState([]);
+
     const villainsInfo = villainsToComments.map(info => {
         return (
             <option value={info.name}>{info.name}</option>
         )
     })
 
+
     function submitForm(e) {
         e.preventDefault()
         console.log("helo")
+        commentsToVillain()
     }
+
+    useEffect(()=>{
+    fetch("http://localhost:3000/comments")
+    .then(response => response.json())
+    .then(commentsData => {console.log("Comments-Fetched:", commentsData)
+
+    setComments(commentsData)
+    })
+    }, [])
+    
+
+
+    // const commentFound=()=>{
+
+    // }
+
+    // }
+
+    const commentsToVillain=()=>{
+        fetch("http://localhost:3000/comments", {method: 'Post', headers:{'Content-Type': 'application/json'}, body: JSON.stringify(
+            {name:villainSelected, comments:commentContent}
+        )})
+    .then(response => response.json())
+    .then(commentsFetched => {console.log("Post-Fetch: ", commentsFetched)
+        setComments([...comments, commentsFetched])
+    })
     
     
+    }
+
+    function handleComment(e){
+        // console.log(e.target.value)
+        setcommentContent(e.target.value)
+        console.log(commentContent)
+    }
+
+     function selectedVillain(e){
+        // console.log(e.target.value)
+        setVillainSelected(e.target.value)
+     }
+    
+    const commentsName= comments.map(eachComment => {
+        console.log(eachComment.name)
+        return(<>
+            <h2>{eachComment.name}</h2>
+            <p>{eachComment.comments}</p>
+        </>)
+    })
+
     return (
         <div className="formContainer">
             <form onSubmit={submitForm}>
                 <label for="villainComments">Choose Villain</label>
-                <select id="villainComments" name="villainComments">
+                <select onChange={selectedVillain} id="villainComments" name="villainComments">
                     {villainsInfo}
                 </select>
                 <label for="comments">Comments</label>
-                <textarea id="comments" name="comments" placeholder="Write a comment..." style={{ height: 100 }}></textarea>
+                <textarea onChange={handleComment} id="comments" name="comments" placeholder="Write a comment..." style={{ height: 100 }}></textarea>
                 <input type="submit" value="Submit"></input>
             </form>
+            {commentsName}
         </div>
     )
 }
--- src/componets/VillainsCard.js
+++ src/componets/VillainsCard.js
@@ -26,25 +26,28 @@ const clickHandler=()=>{
         //     <p>Race: {villainToRender.appearance.race}</p>
         // </div>
         <div className="collection">
-             <button onClick={clickHandler}>Go to Jail</button>
         <div className="flipCard">
-           
+
             <div className="flipCardInner">
                 <div className="flipCardFront">
                     <h2>{villainToRender.name}</h2>
                     <img src={villainToRender.image} className="image" />
+                    <p>Height: {villainToRender.appearance.height}</p>
+                    <p>Weight: {villainToRender.appearance.weight}</p>
+                    <p>Gender: {villainToRender.appearance.gender}</p>
+                    <p>Race: {villainToRender.appearance.race}</p>
                 </div>
-                <div className="flipCardBack">
+                
+                <div className="flipCardBack">                        
+                    <button onClick={clickHandler}>Go to Jail</button>
+
                     <p>Intelligence:{villainToRender.powerstats.intelligence}</p>
                     <p>Strength:{villainToRender.powerstats.strength}</p>
                     <p>Speed:{villainToRender.powerstats.speed}</p>
                     <p>Durability:{villainToRender.powerstats.durability}</p>
                     <p>Power: {villainToRender.powerstats.power}</p>
                     <p>Combat: {villainToRender.powerstats.combat}</p>
-                    <p>Height: {villainToRender.appearance.height}</p>
-                    <p>Weight: {villainToRender.appearance.weight}</p>
-                    <p>Gender: {villainToRender.appearance.gender}</p>
-                    <p>Race: {villainToRender.appearance.race}</p>
+                    
                 </div>
             </div>
             </div>
