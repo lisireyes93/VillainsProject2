6b3611848b65697e677c7c0d10ecea1322487e88
Bryan Gutierrez <gutierrezbryan32@yahoo.com>
2021-08-26 17:52:49 -0400

    FINISHED

---
+++ src/componets/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home(){
+
+    return(
+    <>
+            <h1 className="home">Home</h1>
+            <h3 className="welcome">Welcome</h3>
+            <p className="para">Hello fellow pedestrians! Are you scared for your life everytime you step outside? Cause I am!! This is a website where we can get together to talk to about criminal villain activity. Here we let each other know which villains are out on the run and which villains are currently in jail. We also have a comment section for you to leave concerns, questions, and let others know if a villain is near your neighborhood or to let others know about your experinces with villains.</p>
+            <img className="homeImg" src="https://cdn.goliath.com/eyJidWNrZXQiOiJwdWItc3RvcmFnZSIsImtleSI6ImdvbGlhdGgvd3AtY29udGVudC91cGxvYWRzLzIwMTYvMDEvNDQ3MzM0OS0yNzUwMzUyOTE2LVRoYW5vLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTYwMCwiaGVpZ2h0IjoxMjAwLCJmaXQiOiJjb3ZlciIsImJhY2tncm91bmQiOnsiciI6MCwiZyI6MCwiYiI6MCwiYWxwaGEiOjF9LCJwb3NpdGlvbiI6InRvcCJ9fX0="></img>
+    </>
+    )
+    
+}
+
+export default Home;
--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,33 +1,54 @@
 import React from "react";
 import { ReactDOM } from "react";
 import { NavLink } from 'react-router-dom';
+import Header from './Header';
 
 function NavBar() {
 
     return (
+        <div className="headerNav">
+            <Header />
         <div className="navlinks">
-            <NavLink
+            
+            <button>
+                <NavLink
+                to="/"
+                exact
+            >
+                Home
+            </NavLink>
+            </button>
+            
+            <button>
+                <NavLink
                 to="/jail"
                 exact
             >
-                Jail.
+                Jail
             </NavLink>
-            <NavLink
+            </button>
+
+            <button>
+                 <NavLink
                 to="/wanted"
                 exact
             >
-                Wanted.
+                Wanted
             </NavLink>
-            <NavLink
+            </button>
+           
+           <button>
+              <NavLink
                 to="/comments"
                 exact
             >
-                Comments
-            </NavLink>
-
-
+                    Comments
+                </NavLink> 
+           </button>
+            
 
         </div>
+        </div >
     )
 }
 
